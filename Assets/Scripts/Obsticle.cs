c485708740e094ff0a466db0169ff92145e05119
ben0724 <48838211+ben0724@users.noreply.github.com>
2019-04-09 20:49:40 +0800

    Initial commit

---
+++ Assets/Scripts/Obsticle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obsticle : MonoBehaviour {
+
+
+    public int RotationSpeed;
+
+	void Update () {
+		if(RotationSpeed != 0)
+        {
+            Rotation();
+        }
+	}
+
+    void Rotation()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * RotationSpeed);
+    }
+}
---
+++ Assets/Scripts/ObsticleManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsticleManager : MonoBehaviour {
+
+
+    public GameObject PlayerObj;
+    public GameObject[] ObsticlesArr;
+
+
+    int obsticleCount;
+
+
+    int playerDistanceIndex = -1;
+    int obsticleIndex = 0;
+    int distanceToNext = 50;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        obsticleCount = ObsticlesArr.Length;
+        InstantiateObsticle();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        int PlayerDistance = (int)(PlayerObj.transform.position.y / (distanceToNext));
+
+        if(playerDistanceIndex != PlayerDistance)
+        {
+            InstantiateObsticle();
+            playerDistanceIndex = PlayerDistance;
+        }
+	}
+
+
+    public void InstantiateObsticle()
+    {
+        int RandomInt = Random.Range(0, obsticleCount);
+        GameObject newObsticle = Instantiate(ObsticlesArr[RandomInt], new Vector3(0, obsticleIndex * distanceToNext), Quaternion.identity);
+        newObsticle.transform.SetParent(transform);
+        obsticleIndex++;
+
+    }
+
+
+
+}
